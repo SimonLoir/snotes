1df1c6edc39f1f0bf0328fca505e75f5a076ba63
SimonLoir <simon-loir@hotmail.com>
2019-09-23 17:28:09 +0200

    some tests with pdfjs

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,2 +1,6 @@
 import '../scss/index.scss';
 import isNodeEnv from './tools/env';
+import { PDFJS } from '../js/pdf';
+import '../js/pdf.worker';
+console.log(PDFJS);
+console.log(PDFJS.openDocument);
