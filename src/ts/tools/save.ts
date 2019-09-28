781deccf8254c9a7a204a672334f9f787309be51
SimonLoir <simon-loir@hotmail.com>
2019-09-28 21:24:52 +0200

    first tests with audio

--- src/ts/tools/save.ts
+++ src/ts/tools/save.ts
@@ -12,3 +12,18 @@ export let saveJSONAsFile = (function() {
         window.URL.revokeObjectURL(url);
     };
 })();
+
+export let saveAudioFile = (function() {
+    var a = document.createElement('a');
+    document.body.appendChild(a);
+    a.style.display = 'none';
+    return function(data: any, fileName: string) {
+        var json = data,
+            blob = data,
+            url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    };
+})();
